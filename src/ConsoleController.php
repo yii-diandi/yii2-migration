b93a01e5aa07e39be127ef6a8df43afb494b5d51
途火科技官微 <2192138785@qq.com>
2020-06-24 13:23:00 +0800

    20200624

--- src/ConsoleController.php
+++ src/ConsoleController.php
@@ -3,7 +3,7 @@
  * @Author: Wang chunsheng  email:2192138785@qq.com
  * @Date:   2020-06-23 16:45:55
  * @Last Modified by:   Wang chunsheng  email:2192138785@qq.com
- * @Last Modified time: 2020-06-23 16:46:16
+ * @Last Modified time: 2020-06-24 12:57:42
  */
  
 
@@ -58,6 +58,53 @@ class ConsoleController extends MigrateController
             $migrate = Yii::createObject([
                 'class' => 'diandi\migration\components\MigrateCreate',
                 'migrationPath' => '@app/migrations'
+            ]);
+            $migrate->create($table);
+            unset($migrate);
+        }
+
+        $this->stdout("backup success.\n", Console::FG_GREEN);
+    }
+
+    /**
+     * Creates a new migration. php yii migrate/backup all
+     * 
+     * @param string $name 
+     * @throws Exception if the name argument is invalid.
+     */
+    public function actionAddonsBackup($addonsName){
+        /* 所有数据表 */
+        $alltables  = Yii::$app->db->createCommand("SHOW TABLE STATUS like '%{$addonsName}%'")->queryAll();
+        $alltables  = array_map('array_change_key_case', $alltables);
+        $alltables  = ArrayHelper::getColumn($alltables, 'name');
+
+          /* 备份所有数据 */
+        $tables  = $alltables;
+              
+        $name = trim($addonsName,',');
+        // if ($name == 'all') {
+        //     /* 备份所有数据 */
+        //     $tables  = $alltables;
+        // } else if(strpos($name, ',')){
+        //     /* 备份部分数据表 */
+        //     $tables = explode(',', $name);
+        // } else {
+        //     /* 备份一个数据表 */
+        //     $tables = [$name];
+        // }
+        /* 检查表是否存在 */
+        foreach ($tables as $table) {
+            if (!in_array($table,$alltables)) {
+                $this->stdout($table." table no find ...\n", Console::FG_RED);
+                die();
+            }
+        }
+        /* 创建migration */
+        foreach ($tables as $table) {
+            //$migrate = new MigrateCreate();
+            $migrate = Yii::createObject([
+                'class' => 'diandi\migration\components\MigrateCreate',
+                'migrationPath' => "@common/addons/{$addonsName}/migrations/"
             ]);
             $migrate->create($table);
             unset($migrate);
